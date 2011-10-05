c70bfe5b13d573cbd324ea546d3b14f678cd4536
Joe Walker <jwalker@mozilla.com>
2011-10-05 10:45:44 +0100

    Bug 691012 (chrometest): Use test file from firefox chrome with test harness to make it work in the browser

---
+++ test/harness.js
@@ -0,0 +1,30 @@
+
+var Cu = { import: function() {} };
+function waitForExplicitFinish() {}
+function addTab() {}
+var content = window;
+var browser = {
+  addEventListener: function(name, func, capture) {
+    func();
+  },
+  removeEventListener: function(name, func, capture) {}
+};
+var gBrowser = { removeCurrentTab: function() {} };
+function finish() {
+}
+function is(actual, expected, name) {
+  console.assert(actual === expected, name + ': actual=', actual, ' expected=', expected);
+}
+function ok(test, name) {
+  console.assert(test, name);
+}
+function info() {
+  console.debug.apply(console, arguments);
+}
+function executeSoon(action) {
+  setTimeout(action, 1);
+}
+
+window.addEventListener('load', function() {
+  test();
+});
